90448aee06e6d429c1d570ff1b6c6cd995bdc67f
Taterraster <georgekaragounis122013@gmail.com>
2025-08-21 21:42:52 +0300

    Added standard calculator

--- main.cpp
+++ main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 #include <cmath>
+void standardCalculator(char op,int num1, int num2);
 int main(){
-  std::cout<<"Hello, World!";
+  int operation;
+  std::cout<<"1. Standard Calculator\n";
+  std::cin>>operation;
+  switch(operation){
+    case 1:
+      int num1;
+      int num2;
+      char op;
+      std::cout<<"Enter first number: ";
+      std::cin>>num1;
+      std::cout<<"Enter second number: ";
+      std::cin>>num2;
+      std::cout<<"Enter second operator(+,-,/,*): ";
+      std::cin>>op;
+      standardCalculator(op,num1,num2);
+    break;
+  }
+}
+void standardCalculator(char op,int num1, int num2){
+  switch(op){
+    case'+':
+       std::cout<<"Result: "<<num1+num2;
+    break;
+    case'-':
+       std::cout<<"Result: "<<num1-num2;
+    break;
+    case'*':
+       std::cout<<"Result: "<<num1*num2;
+    break;
+    case'/':
+       std::cout<<"Result: "<<num1/num2;
+    break;
+    default:
+      std::cout<<"Error";
+    break;
+  }
 }
